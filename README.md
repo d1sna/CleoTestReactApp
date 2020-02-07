4a2cb4ef88fc2f979bb13f5b0ce8ca02a680463e
Alexandr <sanyad1sna@gmail.com>
2020-02-07 23:39:15 +0100

    before unsafe rename

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 Test React App for CLEO.one :)
-
-Start App : 'cd app; npm start';
---
+++ src/pages/UserProfile.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Form from "../components/Form";
+import InfoUser from "../components/InfoUser";
+import NavBar from "../components/NavBar";
+
+export default class UserProfile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.submitHandler = this.submitHandler.bind(this);
+
+    this.state = {
+      user: "gaearon"
+    };
+  }
+  submitHandler(event) {
+    event.preventDefault();
+    this.setState({ user: event.target.input.value });
+    event.target.input.value = "";
+    console.log(this.state.user);
+  }
+
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <Form submitHandler={this.submitHandler} />
+        <InfoUser url={"https://api.github.com/users/" + this.state.user} />
+      </div>
+    );
+  }
+}
